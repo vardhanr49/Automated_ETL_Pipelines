5134d5299634c82a3cf2fe7285c26f396e21baf2
Vardhan Menikonda <90019456+vardhanr49@users.noreply.github.com>
2025-07-01 11:16:45 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "sql_db" {
+  name           = var.sql_database_name
+  server_id      = azurerm_mssql_server.sql_server.id
+  sku_name       = "Basic"
+  zone_redundant = false
+}
+
+resource "azurerm_data_factory" "adf" {
+  name                = "adf-devops-pipeline"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
