b353dd1b52bd5b77b715c7211423f412c2ad11bb
Vardhan Menikonda <90019456+vardhanr49@users.noreply.github.com>
2025-07-01 14:04:08 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,7 @@ resource "azurerm_storage_account" "storage" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
+  is_hns_enabled           = true  
 }
 
 resource "azurerm_mssql_server" "sql_server" {
