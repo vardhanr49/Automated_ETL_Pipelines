b5d8adeba2bb446620b77eeb4d6d3bab728cbb5e
Vardhan Menikonda <90019456+vardhanr49@users.noreply.github.com>
2025-07-01 11:18:49 -0500

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "sql_server_name" {
+  value = azurerm_mssql_server.sql_server.name
+}
+
+output "sql_database_name" {
+  value = azurerm_mssql_database.sql_db.name
+}
+
+output "data_factory_name" {
+  value = azurerm_data_factory.adf.name
+}
